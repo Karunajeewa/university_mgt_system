003ed0edf2655bf06d5c89b8a37f56ae19499710
Sunanda Karunajeewa <49464271+Karunajeewa@users.noreply.github.com>
2020-01-31 23:43:07 +0530

    Add files via upload

---
+++ Java_Project.java
@@ -0,0 +1,27 @@
+package java_project;
+
+/**
+ *
+ * @author DELL
+ */
+public class Java_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      //  Login login = new Login();
+      //  login.setVisible(true);
+        
+      //  Panel1 fpanal = new Panel1();
+      //  fpanal.setVisible(true);
+     //   
+     //    Panel2 fPanel2 = new Panel2();
+      //   fPanel2.setVisible(true);
+      
+      newLogin log = new newLogin();
+      log.setVisible(true);
+      
+    
+}
+}
